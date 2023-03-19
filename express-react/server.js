76a2cf66d33cd9d045682f7c7d659708ae81df1c
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-19 15:15:21 +0200

    express-react

--- express-react/server.js
+++ express-react/server.js
@@ -1,11 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Server running"));
+
+app.get("/api/weather", async (req, res) => {
+  const { location } = req.query;
+
+  if (!location || location.trim() === "") {
+    return res.status(400).send("Invalid or missing location parameter");
+  }
+
+  const options = {
+    method: "GET",
+    url: process.env.WEATHER_API_URL,
+    params: { key: process.env.API_KEY, q: location },
+  };
+
+  try {
+    const response = await axios.request(options);
+    const weatherData = response.data;
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching weather data");
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
